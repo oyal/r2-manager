0f0d70815b1b227a4866d0492963cacbbae22052
oyal <oyal616@qq.com>
2024-08-15 16:15:11 +0800

    fix: useSearchParams() should be wrapped in a suspense boundary at page "/"

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
+import { Suspense } from 'react'
+
 import BucketTable from '@/app/_components/bucket-table'
 
 const RootPage = async () => {
   return (
     <div>
       <h1 className="mb-4 text-xl font-bold">文件列表</h1>
-      <BucketTable />
+      <Suspense>
+        <BucketTable />
+      </Suspense>
     </div>
   )
 }
